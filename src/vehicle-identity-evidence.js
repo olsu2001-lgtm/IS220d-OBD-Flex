3412e2c4ab08c1eaf952e3fd95dcdfefb7010bca
olsu2001-lgtm <olsu2001@gmail.com>
2026-09-08 23:18:54 +0300

    feat: add target vehicle identity evidence constants

---
+++ src/vehicle-identity-evidence.js
@@ -0,0 +1,8 @@
+export const IS220D_TARGET_IDENTITY_EVIDENCE = Object.freeze({
+  vehicle: "Lexus IS220d / XE20 / 2AD-FHV",
+  vin: "JTHBB262302028787",
+  calibrationId: "35360000",
+  calibrationVerificationNumberPayloadHex: "01CAD67F74",
+  source: "docs/VEHICLE_EVIDENCE.md",
+  writable: false
+});
